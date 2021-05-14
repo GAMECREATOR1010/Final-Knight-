4465be5edbb6062a8b3538827677d9f008db4709
Qiuqiu0529 <75361726+Qiuqiu0529@users.noreply.github.com>
2021-05-15 01:14:28 +0800

    Delete Resources directory

--- Resources/tileSet1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="tileSet1" tilewidth="64" tileheight="64" tilecount="90" columns="9">
- <image source="tileSet1.png" trans="ff00ff" width="603" height="701"/>
-</tileset>
