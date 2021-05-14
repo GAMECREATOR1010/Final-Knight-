62ca4253b231fddd82e784e6a588fac824046e30
Qiuqiu0529 <75361726+Qiuqiu0529@users.noreply.github.com>
2021-05-15 01:19:45 +0800

    Add files via upload

---
+++ Resources/tileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="tileSet" tilewidth="64" tileheight="64" tilecount="70" columns="10">
+ <image source="tileSet.png" trans="ff00ff" width="640" height="448"/>
+</tileset>
