00e79885fc4a1ebedd73d689abe73c63434d8bff
lphlch <799920264@qq.com>
2021-05-21 23:41:32 +0800

    feat: Create Item Class
    增加物品基类

---
+++ win-build/Item.h
@@ -0,0 +1,26 @@
+/****************************************************************************
+ Copyright (c) 2021 Final-Knight- Group.
+
+ Code version 21w20a.
+
+ 该部分为Item道具类基类头文件（暂定）
+ ****************************************************************************/
+#pragma once
+
+#ifndef __ITEM_H__
+#define __ITEM_H__
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class Item:public Sprite
+{
+public:
+	CREATE_FUNC(Item);
+
+private:
+
+	virtual bool init();
+
+};
+#endif // !__ITEMS_H__
