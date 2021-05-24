b0383e0bb7e46ee10784d48cead68dfc61f2c362
villinale <wangyxin517@163.com>
2021-05-24 21:01:16 +0800

    feat:add resourses filepath
    整合资源文件内容文件路径

---
+++ Classes/source.h
@@ -0,0 +1,20 @@
+/****************************************************************************
+ Copyright (c) 2021 Final-Knight- Group.
+
+ Code version 21w20a (2021-5-24)
+
+ Annotation:所有相关图片及音频获取地址
+			格式: s_X_XXX
+			s为source标注
+			X为类型: P为图片
+					 M为音频
+					 (...待补充)
+			XXX为命名
+ ****************************************************************************/
+#pragma once
+static const char s_P_SetupButton[]         = "buttons/SetupButton.png";
+static const char s_P_SetupButtonSelected[] = "buttons/SetupButtonSelected.png";
+static const char s_P_StopButton[]          = "buttons/StopButton.png";
+static const char s_P_StopButtonSelected[]  = "buttons/StopButtonSelected.png";
+static const char s_P_Start[]               = "scenePictures/Start.png";
+static const char s_M_StartBGM[]            = "bgm/StartBGM.mp3"; 
