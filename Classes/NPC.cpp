7a57b1ccdcce0289fc2d312e3f9312399d4364a8
lphlch <799920264@qq.com>
2021-05-26 21:13:56 +0800

    feat: NPC class
    新增NPC类

---
+++ Classes/NPC.cpp
@@ -0,0 +1,38 @@
+/****************************************************************************
+ Copyright (c) 2021 Final-Knight- Group.
+
+ Code version 21w21a (2021-5-26)
+ ****************************************************************************/
+
+#include "NPC.h"
+
+NPC* NPC::create(int id)
+{
+	auto pRet = new(std::nothrow) NPC();
+	if (pRet && pRet->init(id))
+	{
+		pRet->autorelease();
+		return pRet;
+	}
+	else
+	{
+		delete pRet;
+		pRet = nullptr;
+		return nullptr;
+	}
+
+	return nullptr;
+}
+
+bool NPC::init(int id)
+{
+	/* 如果对应的NPC功能定义及图片存在 */
+	if (initWithFile(FileName))
+	{
+		_id = id;
+		return true;
+	}
+
+
+	return false;
+}
---
+++ Classes/NPC.h
@@ -0,0 +1,37 @@
+/****************************************************************************
+ Copyright (c) 2021 Final-Knight- Group.
+
+ Code version 21w21a (2021-5-26)
+
+ 此部分尚未完成，请勿使用
+
+ 该部分为NPC
+ 不同的NPC有不同的功能
+ 根据不同的id值创建不同的npc
+ ****************************************************************************/
+
+
+
+#pragma once
+#ifndef __NPC_H__
+#define __NPC_H__
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class NPC : public Sprite
+{
+public:
+	CREATE_FUNC(NPC);
+	static NPC* create(int id);
+
+private:
+	int _id;
+	virtual bool init(int id=0);
+};
+
+
+#endif // !__NPC_H__
+
+
+
