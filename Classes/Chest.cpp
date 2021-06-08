7bf5e0bb1d15ab626fc6e25e35cafe6e0f4e5867
lphlch <799920264@qq.com>
2021-06-08 18:45:15 +0800

    feat: new chest class
    新增宝箱类

---
+++ Classes/Chest.cpp
@@ -0,0 +1,78 @@
+/****************************************************************************
+ Copyright (c) 2021 Final-Knight- Group.
+
+ 该部分为宝箱类
+ ****************************************************************************/
+#include "Chest.h"
+
+/// <summary>
+/// 仅添加物理碰撞
+/// </summary>
+/// <returns></returns>
+bool Chest::init()
+{
+	/* 添加碰撞范围 */
+	auto phy = PhysicsBody::createCircle(_physicalRange);
+	SetBody(phy, ItemCate);
+	addComponent(phy);
+
+	return true;
+}
+
+WhiteChest* WhiteChest::create()
+{
+	auto pRet = new(std::nothrow) WhiteChest();
+	if (pRet && pRet->init())
+	{
+		pRet->autorelease();
+		return pRet;
+	}
+	else
+	{
+		delete pRet;
+		pRet = nullptr;
+	}
+	return nullptr;
+}
+
+Item * WhiteChest::open()
+{
+	/* 动画效果（待定） */
+
+	/* 改变材质 */
+
+	/* 随机生成 */
+	int type = random(1, 5);
+	switch (type)
+	{
+		case 1:	//生成金币和能量
+			goldMoney->ChangeBalance(2);
+			mand-> + 2;
+			return nullptr;
+		case 2:	//生成HP药水
+			auto hp=HealPotion::create(SMALL);
+			return hp;
+		case 3:	//生成MP药水
+			auto mp = ManaPotion::create(SMALL);
+			return mp;
+		case 4:	//生成武器
+			
+		default:
+			throw("No such type in chest");
+			break;
+	}
+}
+
+/// <summary>
+/// 添加图片，添加碰撞范围
+/// </summary>
+/// <returns></returns>
+bool WhiteChest::init()
+{
+	if (!initWithFile(WHITE_CHEST_PATH) && !Chest::init())
+	{
+		return false;
+	}
+
+	return true;
+}
---
+++ Classes/Chest.h
@@ -0,0 +1,54 @@
+/****************************************************************************
+ Copyright (c) 2021 Final-Knight- Group.
+
+ 该部分为宝箱类
+ ****************************************************************************/
+#pragma once
+
+#ifndef __CHEST_H__
+#define __CHEST_H__
+
+#include "cocos2d.h"
+#include "Const.h"
+#include "Money.h"
+#include "Potion.h"
+
+USING_NS_CC;
+
+const char* WHITE_CHEST_PATH = "/chests/WhiteChest.png";
+const char* BLUE_CHEST_PATH = "/chests/BlueChest.png";
+const char* BROWN_CHEST_PATH = "/chests/BrownChest.png";
+
+enum ChestType
+{
+	WHITE_CHEST,
+	BLUE_CHEST,
+	BROWNCHEST,
+
+	CHEST_COUNT
+};
+
+class Chest : public Sprite
+{
+public:
+	virtual Item * open() = 0;
+protected:
+	virtual bool init();
+private:
+	static constexpr double _physicalRange = 80.0;
+
+};
+
+class WhiteChest : public Chest
+{
+public:
+	static WhiteChest* create();
+	virtual Item * open();
+private:
+	virtual bool init();
+
+
+};
+#endif // !__CHEST_H__
+
+
