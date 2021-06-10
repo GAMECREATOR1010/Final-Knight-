98d0372ba99ed0ab63bf593d8942670e9371deee
villinale <wangyxin517@163.com>
2021-06-10 23:17:15 +0800

    build:edit and delete some file paths
    编辑和删除相关文件路径

--- Classes/source.h
+++ Classes/source.h
@@ -13,10 +13,12 @@
  ****************************************************************************/
 #pragma once
 static const char s_P_Start[]                 = "scenePictures/Start.png";
+static const char s_P_StopFrame[]             = "scenePictures/StopFrame";
 
 static const char s_P_SetupButton[]           = "buttons/SetupButton.png";
 static const char s_P_SetupButtonSelected[]   = "buttons/SetupButtonSelected.png";
 static const char s_P_SetupButtonSelectedBG[] = "buttons/SetupButtonSelectedBG.png";
+
 static const char s_P_BGMButton[]             = "buttons/BGMButton.png";
 static const char s_P_BGMButtonSelected[]     = "buttons/BGMButtonSelected.png";
 static const char s_P_SEButton[]              = "buttons/SEButton.png";
@@ -26,6 +28,5 @@ static const char s_P_StopButton[]            = "buttons/StopButton.png";
 static const char s_P_StopButtonSelectedBG[]  = "buttons/StopButtonSelectedBG.png";
 
 
-
 static const char s_M_StartBGM[]              = "bgm/StartBGM.mp3";
-static const char s_M_ButtonSelected[]        = "bgm/ButtonSelected.wav";
+static const char s_M_ButtonSelected[]        = "bgm/ButtonSelected.mp3";
