42eb8470262389a077d69e9f5ce0131123f85bcf
Qiuqiu0529 <75361726+Qiuqiu0529@users.noreply.github.com>
2021-06-16 18:07:34 +0800

    feat:changeHP、energy

--- Classes/Knight.cpp
+++ Classes/Knight.cpp
@@ -4,7 +4,7 @@ USING_NS_CC;
 Knight* Knight::create(int type, int rank)
 {
 	Knight* knight = new Knight;
-	if (knight != nullptr && knight->init(type, rank))
+	if (knight != nullptr && knight->init( type, rank))
 	{
 		knight->autorelease();
 		return knight;
@@ -13,40 +13,49 @@ Knight* Knight::create(int type, int rank)
 	return nullptr;
 }
 
+
+
 bool Knight::init(int type, int rank)
 {
+	wea1 = nullptr;
 	state = KnightIdle;
 	body = PhysicsBody::create();
 	if (type == 0)
 	{
-		maxHP = 5;
-		HP = 5;
+		maxHP = 100;
+		HP = 100;
 		pic = Sprite::create("Knight1.png");
 		addChild(pic, 0);
-		auto shape = PhysicsShapeBox::create(Size(45.0f, 40.0f));
+		auto shape = PhysicsShapeBox::create(Size(45.0f,40.0f));
 		body->addShape(shape);
 		pic->setGlobalZOrder(shadeOrder);
 		pic->setAnchorPoint(Vec2(0.7f, 0.6f));
 		attackMode = gunEnum;
-
+		
 		Animation* animation = Animation::create();
 		animation->addSpriteFrameWithFile("Knight1.png");
 		animation->addSpriteFrameWithFile("Knight2.png");
 		animation->setDelayPerUnit(0.5);
 		Animate* Aniaction = Animate::create(animation);
 		pic->runAction(RepeatForever::create(Aniaction));
-
-		AddShade(Vec2(-5, -40));
-		wea = Weapon::create(6, KnightCate);
+		
+		AddShade( Vec2(-5,-40));
+		wea = Weapon::create(9, KnightCate);
 		addChild(wea);
 		wea->setPosition(wea->bindPoint);
-	}
+   }
 
 	body->setDynamic(false);
 	addComponent(body);
 	SetBody(body, KnightCate);
 	setTag(knightTag);
 	return true;
+}
+
+void Knight::LevelUp()
+{
+	energyMax += 10;
+	energyNow = energyMax;
 }
 
 void Knight::AddDefence(float addDefence)
@@ -59,19 +68,50 @@ void Knight::AddMoveSpeed(float addMoveSpeed)
 	moveSpeed += addMoveSpeed;
 }
 
+void Knight::HPNowChange(float hpchange)
+{
+	HP+= hpchange;
+}
+
+void Knight::HPMaxChange(float hpchange)
+{
+	maxHP += hpchange;
+}
+
+void Knight::EnergyMaxChange(float energychange)
+{
+	energyMax += energychange;
+}
+void Knight::EnergyNowChange(float energychange)
+{
+	energyNow += energychange;
+}
+
+
+void Knight::AddEXP(int addEXP)
+{
+	EXP += addEXP;
+	if (EXP / 20 > rank * 20)
+	{
+		rank += 1;
+		LevelUp();
+	}
+}
+
+
 void Knight::MyAttack()
 {
-	if (this->state != KnightAttack && (energy - wea->costEnergy) > 0)
+	if (this->state != KnightAttack&&(energyNow-wea->costEnergy)>=0)
 	{
 		this->state = KnightAttack;
-		energy -= wea->costEnergy;
+		energyNow -= wea->costEnergy;
 		auto attacking = CallFunc::create([&]() {
 			wea->WeaponAttack(this->faceDir);
 			});
 		auto attackEnd = CallFunc::create([&]() {
 			this->state = KnightIdle;
 			});
-		auto delay = DelayTime::create(0.5f);
+		auto delay = DelayTime::create(wea->perTime);
 		auto myseq = Sequence::create(attacking, delay, attackEnd, nullptr);
 		this->runAction(myseq);
 	}
--- Classes/Knight.h
+++ Classes/Knight.h
@@ -9,15 +9,23 @@ USING_NS_CC;
 class Knight :public Actor
 {
 public:
-	static Knight* create(int type = 0, int rank = 1);
-	KnightState state;
-	void MyAttack();
-	void AddDefence(float addDefence);
-	void AddMoveSpeed(float addMoveSpeed);
-
+    static Knight* create(int type=0,int rank=1);
+    KnightState state;
+    void MyAttack();
+    void AddDefence(float addDefence);
+    void AddMoveSpeed(float addMoveSpeed);
+    void LevelUp();
+    void AddEXP(int );
+    void HPNowChange(float hpchange);
+    void HPMaxChange(float hpchange);
+    void EnergyMaxChange(float energychange);
+    void EnergyNowChange(float energychange);
 private:
-	virtual bool init(int type = 0, int rank = 1);
-	int energy = 150;
+    virtual bool init(int type=0, int rank=1);
+    int energyMax=150;
+    int energyNow=1000;
+    int rank = 1;
+    int EXP = 0;
 };
 
-#endif
+#endif 
