8215acccc8c4afd98c51d7e07119e50ee39df75c
Qiuqiu0529 <75361726+Qiuqiu0529@users.noreply.github.com>
2021-06-17 23:46:39 +0800

    feat：more  attribute  change

--- Classes/Knight.cpp
+++ Classes/Knight.cpp
@@ -22,7 +22,7 @@ bool Knight::init(int type, int rank)
 	body = PhysicsBody::create();
 	if (type == 0)
 	{
-		maxHP = 50;
+		HPMax = 50;
 		HP = 50;
 		pic = Sprite::create("Knight1.png");
 		addChild(pic, 0);
@@ -44,7 +44,7 @@ bool Knight::init(int type, int rank)
 		addChild(wea);
 		wea->setPosition(wea->bindPoint);
    }
-
+	moveSpeed = moveSpeedMax;
 	body->setDynamic(false);
 	addComponent(body);
 	SetBody(body, KnightCate);
@@ -52,42 +52,285 @@ bool Knight::init(int type, int rank)
 	return true;
 }
 
+
+
+void Knight::HPNowChange(float hpchange,float continueTime)
+{
+	if (continueTime == -1.0f)
+	{
+		if (HP + hpchange <= HPMax)
+		{
+			HP += hpchange;
+		}
+		else
+		{
+			HP = HPMax;
+		}
+	}
+	else if(continueTime>0.0f)
+	{
+		auto delay = DelayTime::create(continueTime);
+		tempHP = HP;
+		HP += hpchange;
+		auto recovery = CallFunc::create([&]() {
+			HP = tempHP;
+			});
+		auto seq = Sequence::create(delay, recovery, nullptr);
+		runAction(seq);
+	}
+}
+void Knight::HPMaxChange(float hpchange)
+{
+	HPMax += hpchange;
+	HP = HPMax;
+}
+
+
+void Knight::DefenceNowChange(float defencechange, float continueTime)
+{
+	if (continueTime == -1.0f)
+	{
+		if (defence + defencechange <= defenceMax)
+		{
+			defence += defencechange;
+		}
+		else
+		{
+			defence = defenceMax;
+		}
+	}
+	else if (continueTime > 0.0f)
+	{
+		auto delay = DelayTime::create(continueTime);
+		tempDefence = defence;
+		defence += defencechange;
+		auto recovery = CallFunc::create([&]() {
+			defence = tempDefence;
+			});
+		auto seq = Sequence::create(delay, recovery, nullptr);
+		runAction(seq);
+	}
+}
+void Knight::DefenceMaxChange(float defencechange)
+{
+	defenceMax += defencechange;
+	defence = defenceMax;
+}
+
+
+void Knight::MoveSpeedNowChange(float moveSpeedchange, float continueTime)
+{
+	if (continueTime == -1.0f)
+	{
+		if (moveSpeed + moveSpeedchange <= moveSpeedMax)
+		{
+			moveSpeed += moveSpeedchange;
+		}
+		else
+		{
+			moveSpeed = moveSpeedMax;
+		}
+	}
+	else if (continueTime > 0.0f)
+	{
+		auto delay = DelayTime::create(continueTime);
+		tempMoveSpeed = moveSpeed;
+		moveSpeed += moveSpeedchange;
+		auto recovery = CallFunc::create([&]() {
+			moveSpeed = tempMoveSpeed;
+			});
+		auto seq = Sequence::create(delay, recovery, nullptr);
+		runAction(seq);
+	}
+}
+void Knight::MoveSpeedMaxChange(float moveSpeedchange)
+{
+	moveSpeedMax += moveSpeedchange;
+	moveSpeed = moveSpeedMax;
+}
+
+
+void Knight::EnergyMaxChange(float energychange)
+{
+	energyMax += energychange;
+	energyNow = energyMax;
+}
+void Knight::EnergyNowChange(float energychange)
+{
+	if (energyNow + energychange <= energyMax)
+	{
+		energyNow += energychange;
+	}
+	else
+	{
+		energyNow = energyMax;
+	}
+}
+
+void Knight::DamageNowChange(float damagechange, float continueTime)
+{
+	if (continueTime == -1.0f)
+	{
+		if (damage + damagechange <= damageMax)
+		{
+			damage += damagechange;
+		}
+		else
+		{
+			damage = damageMax;
+		}
+		wea->SetDamageBuff(damage);
+	}
+	else if (continueTime > 0.0f)
+	{
+		auto delay = DelayTime::create(continueTime);
+		tempDamage = damage;
+		damage += damagechange;
+		wea->SetDamageBuff(damage);
+		auto recovery = CallFunc::create([&]() {
+			damage = tempDamage;
+			wea->SetDamageBuff(damage);
+			});
+		auto seq = Sequence::create(delay, recovery, nullptr);
+		runAction(seq);
+	}
+}
+void Knight::DamageMaxChange(float damagechange)
+{
+	damageMax += damagechange;
+	damage = damageMax;
+	wea->SetDamageBuff(damage);
+
+}
+
+
+void Knight::AttackSpeedChange(float  attackSpeedchange, float continueTime)
+{
+	if (continueTime == -1.0f)
+	{
+		if (attackSpeed + attackSpeedchange <= attackSpeedMax)
+		{
+			attackSpeed += attackSpeedchange;
+		}
+		else
+		{
+			attackSpeed = attackSpeedMax;
+		}
+		wea->SetSpeedBuff(attackSpeed);
+	}
+	else if (continueTime > 0.0f)
+	{
+		auto delay = DelayTime::create(continueTime);
+		tempAttackSpeed = attackSpeed;
+		attackSpeed += attackSpeedchange;
+		wea->SetSpeedBuff(attackSpeed);
+		auto recovery = CallFunc::create([&]() {
+			attackSpeed = tempAttackSpeed;
+			wea->SetSpeedBuff(attackSpeed);
+			});
+		auto seq = Sequence::create(delay, recovery, nullptr);
+		runAction(seq);
+	}
+}
+void Knight::AttackSpeedMaxChange(float attackSpeedchange)
+{
+	attackSpeedMax += attackSpeedchange;
+	attackSpeed = attackSpeedMax;
+	wea->SetSpeedBuff(attackSpeed);
+}
+
+void Knight::AttackRangeChange(float attackRangechange, float continueTime )
+{
+	if (continueTime == -1.0f)
+	{
+		if (attackRange + attackRangechange <= attackRangeMax)
+		{
+			attackRange += attackRangechange;
+		}
+		else
+		{
+			attackRange = attackRangeMax;
+		}
+		wea->SetRangeBuff(attackRange);
+	}
+	else if (continueTime > 0.0f)
+	{
+		auto delay = DelayTime::create(continueTime);
+		tempAttackRange = attackRange;
+		attackRange += attackRangechange;
+		wea->SetRangeBuff(attackRange);
+		auto recovery = CallFunc::create([&]() {
+			attackRange = tempAttackRange;
+			wea->SetRangeBuff(attackRange);
+			});
+		auto seq = Sequence::create(delay, recovery, nullptr);
+		runAction(seq);
+	}
+}
+void Knight::AttackRangeMaxChange(float attackRangechange)
+{
+	attackRangeMax += attackRangechange;
+	attackRange = attackRangeMax;
+	wea->SetRangeBuff(attackRange);
+}
+
+
+void Knight::AttackDistanceChange(float attackDistancechange, float continueTime)
+{
+	if (continueTime == -1.0f)
+	{
+		if (attackDistance + attackDistancechange <= attackDistanceMax)
+		{
+			attackDistance += attackDistancechange;
+		}
+		else
+		{
+			attackDistance = attackDistanceMax;
+		}
+		wea->SetDistanceBuff(attackDistance);
+	}
+	else if (continueTime > 0.0f)
+	{
+		auto delay = DelayTime::create(continueTime);
+		tempAttackDistance = attackDistance;
+		attackDistance += attackDistancechange;
+		wea->SetDistanceBuff(attackDistance);
+		auto recovery = CallFunc::create([&]() {
+			attackDistance = tempAttackDistance;
+			wea->SetDistanceBuff(attackDistance);
+			});
+		auto seq = Sequence::create(delay, recovery, nullptr);
+		runAction(seq);
+	}
+}
+void Knight::AttackDistanceMaxChange(float attackDistancechange)
+{
+	attackDistanceMax += attackDistancechange;
+	attackDistance = attackDistanceMax;
+	wea->SetDistanceBuff(attackDistance);
+}
+
+float Knight::GetEnergyNow()
+{
+	return energyNow;
+}
+
+float Knight::GetEnergyMax()
+{
+	return energyMax;
+}
+
+int Knight::GetRank()
+{
+	return rank;
+}
+
+
 void Knight::LevelUp()
 {
 	energyMax += 10;
 	energyNow = energyMax;
 }
-
-
-void Knight::AddDefence(float addDefence)
-{
-	defence += addDefence;
-}
-
-void Knight::AddMoveSpeed(float addMoveSpeed)
-{
-	moveSpeed += addMoveSpeed;
-}
-
-void Knight::HPNowChange(float hpchange)
-{
-	HP+= hpchange;
-}
-
-void Knight::HPMaxChange(float hpchange)
-{
-	maxHP += hpchange;
-}
-
-void Knight::EnergyMaxChange(float energychange)
-{
-	energyMax += energychange;
-}
-void Knight::EnergyNowChange(float energychange)
-{
-	energyNow += energychange;
-}
-
 
 void Knight::AddEXP(int addEXP)
 {
@@ -98,7 +341,6 @@ void Knight::AddEXP(int addEXP)
 		LevelUp();
 	}
 }
-
 
 void Knight::MyAttack()
 {
--- Classes/Knight.h
+++ Classes/Knight.h
@@ -12,18 +12,47 @@ public:
     static Knight* create(int type=0,int rank=1);
     KnightState state;
     void MyAttack();
-    void AddDefence(float addDefence);
-    void AddMoveSpeed(float addMoveSpeed);
     void LevelUp();
-    void AddEXP(int );
-    void HPNowChange(float hpchange);
-    void HPMaxChange(float hpchange);
-    void EnergyMaxChange(float energychange);
-    void EnergyNowChange(float energychange);
+    void AddEXP(int);
+
+    
+
+    void HPNowChange(float,float continueTime= -1.0f);
+    void HPMaxChange(float);
+
+    void DefenceNowChange(float, float continueTime = -1.0f);
+    void DefenceMaxChange(float);
+  
+    void MoveSpeedNowChange(float, float continueTime = -1.0f);
+    void MoveSpeedMaxChange(float );
+
+    void DamageNowChange(float, float continueTime = -1.0f);
+    void DamageMaxChange(float);
+
+    void AttackSpeedChange(float, float continueTime = -1.0f);
+    void AttackSpeedMaxChange(float);
+
+    void AttackRangeChange(float, float continueTime = -1.0f);
+    void AttackRangeMaxChange(float);
+
+    void AttackDistanceChange(float, float continueTime = -1.0f);
+    void AttackDistanceMaxChange(float);
+
+    void EnergyNowChange(float);
+    void EnergyMaxChange(float);
+
+    float GetEnergyNow();
+    float GetEnergyMax();
+    int  GetRank();
+
 private:
     virtual bool init(int type=0, int rank=1);
-    int energyMax=150;
-    int energyNow=1000;
+    float  energyMax=150;
+    float  energyNow=150;
+
+    float tempHP, tempEnergy, tempDefence, tempMoveSpeed,tempDamage,
+        tempAttackSpeed, tempAttackRange,  tempAttackDistance;
+
     int rank = 1;
     int EXP = 0;
 };
