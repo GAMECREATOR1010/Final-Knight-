573cbcc813c4d0c861eef14844c518555e4774f8
vvvi <wangyxin517@163.com>
2021-06-20 08:27:37 +0800

    feat:add blood UI
    增加血条显示

--- Classes/Layer/BloodLayer.cpp
+++ Classes/Layer/BloodLayer.cpp
@@ -11,11 +11,55 @@
 USING_NS_CC;
 using namespace cocos2d::ui;
 
-static void problemLoading(const char* filename)
+
+
+void UIItem::AddStuff(Sprite* sprite, float now, float max)
 {
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
+    _sprite = sprite;
+    _now = now;
+    _max = max;
+    char string[30] = { 0 };
+    sprintf(string, "%d/%d", (int)_now, (int)_max);
+    _label = Label::createWithTTF(string, s_T_Pixeboy, 25);
+
+    float precen = now / max;
+    _sprite->setScaleX(precen);
+    _sprite->setGlobalZOrder(uiOrder);
+    _sprite->setAnchorPoint(Vec2(0, 1));
+
+    _label->setGlobalZOrder(uiOrder);
 }
+
+Sprite* UIItem::GetSprite()
+{
+    return _sprite;
+}
+Label* UIItem::GetLabel()
+{
+    return _label;
+}
+
+void UIItem::SetPosition(float x, float y)
+{
+    _sprite->setPosition(x, y);
+    float x_lab = x + _sprite->getContentSize().width / 2;
+    float y_lab = y - _sprite->getContentSize().height / 2;
+    _label->setPosition(x_lab, y_lab);
+}
+
+
+void UIItem::Change(float now, float max)
+{
+    _now = now;
+    _max = max;
+    float precen = now / max;
+    _sprite->setScale(precen);
+    char string[30] = { 0 };
+    sprintf(string, "%d/%d", (int)_now, (int)_max);
+    _label->setString(string);
+}
+
+
 
 bool BloodLayer::init()
 {
@@ -27,25 +71,61 @@ bool BloodLayer::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     auto originSize = Director::getInstance()->getVisibleOrigin();
 
-    _MP = Sprite::create(s_P_MPUI);
-    _DF = Sprite::create(s_P_DFUI);
-    _HP = Sprite::create(s_P_HPUI);
     {
         //ÏÔÊ¾±³¾°Í¼Æ¬
         auto bloodFrame = Sprite::create(s_P_BloodFrame);
-        if (bloodFrame == nullptr)
-        {
-            problemLoading(s_P_BloodFrame);
-        }
-        else
-        {
-            float x = bloodFrame->getContentSize().width / 2 + originSize.x;
-            float y= visibleSize.height- bloodFrame->getContentSize().height / 2 + originSize.y;
-            bloodFrame->setPosition(x, y);
-            bloodFrame->setGlobalZOrder(uiOrder);
-            this->addChild(bloodFrame);
-        }
+        float x = bloodFrame->getContentSize().width / 2 + originSize.x;
+        float y = visibleSize.height - bloodFrame->getContentSize().height / 2 + originSize.y;
+        bloodFrame->setPosition(x, y);
+        bloodFrame->setGlobalZOrder(uiOrder);
+        this->addChild(bloodFrame);
+
     }
 
     return true;
+}
+
+
+void BloodLayer::SetKnight(Knight& kinght)
+{
+    auto visibleSize = Director::getInstance()->getVisibleSize();
+    auto originSize = Director::getInstance()->getVisibleOrigin();
+
+
+
+    {
+        float x = BloodUIx + originSize.x;
+        float y = visibleSize.height - BloodUIy + originSize.y;
+
+        if (hp.GetSprite()==nullptr)
+        {
+            hp.AddStuff(Sprite::create(s_P_HPUI), kinght.GetHP(), kinght.GetHPMax());
+            df.AddStuff(Sprite::create(s_P_DFUI), kinght.GetDefence(), kinght.GetDefenceMax());
+            mp.AddStuff(Sprite::create(s_P_MPUI), kinght.GetEnergyNow(), kinght.GetEnergyMax());
+        }
+
+        Change(kinght);
+
+    
+        hp.SetPosition(x, y);
+        addChild(hp.GetSprite());
+        addChild(hp.GetLabel());
+
+        y -= BloodUIGap;
+        df.SetPosition(x, y);
+        addChild(df.GetSprite());
+        addChild(df.GetLabel());
+
+        y -= BloodUIGap;
+        mp.SetPosition(x, y);
+        addChild(mp.GetSprite());
+        addChild(mp.GetLabel());
+    }
+}
+
+void BloodLayer::Change(Knight& kinght)
+{
+    hp.Change(kinght.GetHP(), kinght.GetHPMax());
+    df.Change(kinght.GetDefence(), kinght.GetDefenceMax());
+    mp.Change(kinght.GetEnergyNow(), kinght.GetEnergyMax());
 }
--- Classes/Layer/BloodLayer.h
+++ Classes/Layer/BloodLayer.h
@@ -13,11 +13,42 @@
 #define __BLOOD_LAYER_H__
 
 #include "cocos2d.h"
-#include "source.h" 
-#include "Const.h"
-#include "ui/CocosGUI.h"
 #include "Knight.h"
+#include "source.h" 
 
+
+class UIItem
+{
+public:
+    void AddStuff(Sprite* sprite, float now, float max);
+    void SetPosition(float x, float y);
+    void Change(float now, float max);
+
+    Sprite* GetSprite();
+    Label* GetLabel();
+    Sprite* _sprite;
+    Label* _label;
+    float _now ;
+    float _max ;
+};
+
+static class HP :public UIItem
+{
+public:
+private:
+}hp;
+
+static  class DF :public UIItem
+{
+public:
+private:
+}df;
+
+static  class MP :public UIItem
+{
+public:
+private:
+}mp;
 
 class BloodLayer : public cocos2d::Layer
 {
@@ -25,13 +56,23 @@ public:
 
     virtual bool init();
 
-    CREATE_FUNC(BloodLayer);
+    static  BloodLayer* create()
+    {
+        auto ret = new (std::nothrow)  BloodLayer();
+        if (ret && ret->init())
+        {
+            ret->autorelease();
+        }
+        else
+        {
+            CC_SAFE_DELETE(ret);
+        }
+        return ret;
+    }
 
-private:
-    Sprite* _DF;
-    Sprite* _MP;
-    Sprite* _HP;
+    void SetKnight(Knight& kinght);
 
+    static void Change(Knight& kinght);
 };
 
 #endif __BLOOD_LAYER_H__
