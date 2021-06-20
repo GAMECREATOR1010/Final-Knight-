1162fa3295d81773fc4ba181c5473a30f5ea5993
lphlch <799920264@qq.com>
2021-06-20 20:45:18 +0800

    feat: chest item change
    宝箱物品概率变更

--- Classes/Chest.cpp
+++ Classes/Chest.cpp
@@ -51,11 +51,20 @@ Item* WhiteChest::open(Knight* activer)
 	auto seq = Sequence::create(Animate::create(animation), recovery, nullptr);
 	this->runAction(seq);
 
+	int ran = random(1, 100);
+	if (ran >= 95)
+	{
+		auto wp = Weapon::create();
+	}
+	else if (ran >= 85)
+	{
+		auto potion = FullPotion::create(SMALL);
+	}
 	goldMoney.ChangeBalance(MONEY_EVERY_ROOM * MONEY_LEVEL_MUTI);
 	activer->EnergyNowChange(MANA_EVERY_ROOM * MANA_LEVEL_MUTI);
 	CCLOG("WhiteChest::open get gold %d", MONEY_EVERY_ROOM * MONEY_LEVEL_MUTI);
-	return nullptr;
 
+	return nullptr;
 }
 
 /// <summary>
